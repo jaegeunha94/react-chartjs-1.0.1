51aab5edbf43773a52f541fc35043118c0fef843
jaegeunha94 <jaegeunha94@gmail.com>
2023-10-24 16:58:42 +0900

    Add Dogunut pie

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './style.css';
 
 import CharBar from './CharBar';
 import BubbleChart from './Bubble';
+import DoughnutChart from './Dogunut';
 
 export default function App() {
   return (
     <div>
       <CharBar />
       <BubbleChart />
+      <DoughnutChart />
     </div>
   );
 }
