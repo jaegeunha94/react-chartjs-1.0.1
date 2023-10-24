9316569c60df6d4e074046a29275ea6f99405586
jaegeunha94 <jaegeunha94@gmail.com>
2023-10-24 14:54:23 +0900

    asd

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './style.css';
 
 import CharBar from './CharBar';
+import BubbleChart from './Bubble';
 
 export default function App() {
   return (
     <div>
       <CharBar />
+      <BubbleChart />
     </div>
   );
 }
