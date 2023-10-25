839aa0156adadd08e1a03c0a0afd5b8df92a92eb
jaegeunha94 <jaegeunha94@gmail.com>
2023-10-25 15:22:10 +0900

    Add Radar Chart

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CharBar from './CharBar';
 import BubbleChart from './Bubble';
 import DoughnutChart from './Dogunut';
 import PieChart from './Pie';
+import RadarChart from './Rador';
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
       <BubbleChart />
       <DoughnutChart />
       <PieChart />
+      <RadarChart/>
     </div>
   );
 }
